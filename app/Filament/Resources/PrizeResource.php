19a3371126b5e8cb610f082df5f06f82bb2d689a
puridev1155 <92970876+puridev1155@users.noreply.github.com>
2024-07-10 12:24:57 +0900

    label 내용 변경

--- app/Filament/Resources/PrizeResource.php
+++ app/Filament/Resources/PrizeResource.php
@@ -26,30 +26,39 @@ class PrizeResource extends Resource
                 Forms\Components\TextInput::make('vol')
                     ->numeric(),
                 Forms\Components\TextInput::make('award')
+                    ->label('상품')
                     ->maxLength(255),
                 Forms\Components\TextInput::make('name')
+                    ->label('이름')
                     ->maxLength(255),
                 Forms\Components\TextInput::make('email')
+                    ->label('이메일')
                     ->email()
                     ->required()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('phone')
+                    ->label('연락처')
                     ->tel()
                     ->required()
                     ->maxLength(13),
                 Forms\Components\TextInput::make('wip')
+                    ->label('IP주소')
                     ->maxLength(255),
                 Forms\Components\TextInput::make('agree')
+                    ->label('약관동의')
                     ->required()
                     ->numeric()
                     ->default(1),
                 Forms\Components\TextInput::make('success')
+                    ->label('성공여부')
                     ->required()
                     ->numeric()
                     ->default(0),
                 Forms\Components\TextInput::make('counts')
+                    ->label('등록수')
                     ->numeric(),
                 Forms\Components\TextInput::make('device')
+                    ->label('기기종류')
                     ->maxLength(255),
                     
             ]);
@@ -65,29 +74,39 @@ class PrizeResource extends Resource
                 Tables\Columns\TextColumn::make('award')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('name')
+                    ->label('이름')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
+                    ->label('이메일')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('phone')
+                    ->label('연락처')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('wip')
+                    ->label('IP주소')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('device')
+                Tables\Columns\TextColumn::make('device')   
+                    ->label('기기종류')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('agree')
+                    ->label('약관동의')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('success')
+                    ->label('성공여부')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('counts')
+                    ->label('등록수')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
+                    ->label('등록일')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->label('수정일')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
