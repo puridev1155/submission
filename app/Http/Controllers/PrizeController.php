a3994e53f0eb50b20e6b97e842ff99be2fd05d05
puridev1155 <92970876+puridev1155@users.noreply.github.com>
2024-07-10 18:51:03 +0900

    cors update

--- app/Http/Controllers/PrizeController.php
+++ app/Http/Controllers/PrizeController.php
@@ -6,14 +6,19 @@ use Illuminate\Http\Request;
 use App\Models\Prize;
 use App\Http\Controllers\JsonResponse;
 use App\Http\Requests\PrizeRequest;
+use Illuminate\Support\Facades\Log;
 
 class PrizeController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      */
-    public function store(PrizeRequest $request)
+    public function store(Request $request)
     {
+
+        Log::info('Prize Controller Store Method Invoked');
+        Log::info('Request Data: ', $request->all());
+        return $request->all();
         $validated = $request->validated();
         $validated['device'] =  $request->userAgent();
         $validated['wip'] = $request->ip();
